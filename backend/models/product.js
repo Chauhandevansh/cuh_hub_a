136690b04d3d9d6eb0fe305a7ae9a90f789caead
Aban Dubey <abandubey@gmail.com>
2023-10-06 18:24:18 +0530

    set margins

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,59 +1,59 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    productId: {
-        type: String,
-        required: [true,"productId field can't be empty!"]
-      },
-      imageUrl: {
-        type: String,
-        required: [true,"imageUrl field can't be empty!"]
-      },
-      ownerId: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: [true,"ownerId field can't be empty!"]
-      },
-      openinigPrice: {
-        type: Number,
-        required: [true,"openingPrice field can't be empty!"]
-      },
-      productStatus: {
-        type: Boolean,
-        default: true
-      },
-      bidAccepted: {
-        type: Boolean,
-        default: false
-      },
-      topBid: {
-        type: Number,
-        //required: [true,"userId field can't be empty!"]
-      },
-      description: {
-        type: String,
-        //maxlength: 100
-      },
-      productTimestamp: {
-        type: Number,
-        // required: [true,"userId field can't be empty!"]
-      },
-      deadline: {
-        type: Number,
-        // required: [true,"userId field can't be empty!"]
-      },
-      keyword: {
-        type: String,
-        // required: [true,"userId field can't be empty!"]
-      },
-      productType: {
-        type: String,
-        // required: [true,"userId field can't be empty!"]
-      },
-      allBids: {
-        type: Array,
-        default: []
-      },
+  productId: {
+    type: String,
+    required: [true,"productId field can't be empty!"]
+  },
+  imageUrl: {
+    type: String,
+    required: [true,"imageUrl field can't be empty!"]
+  },
+  ownerId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true,"ownerId field can't be empty!"]
+  },
+  openinigPrice: {
+    type: Number,
+    required: [true,"openingPrice field can't be empty!"]
+  },
+  productStatus: {
+    type: Boolean,
+    default: true
+  },
+  bidAccepted: {
+    type: Boolean,
+    default: false
+  },
+  topBid: {
+    type: Number,
+    //required: [true,"userId field can't be empty!"]
+  },
+  description: {
+    type: String,
+    //maxlength: 100
+  },
+  productTimestamp: {
+    type: Number,
+    // required: [true,"userId field can't be empty!"]
+  },
+  deadline: {
+    type: Number,
+    // required: [true,"userId field can't be empty!"]
+  },
+  keyword: {
+    type: String,
+    // required: [true,"userId field can't be empty!"]
+  },
+  productType: {
+    type: String,
+    // required: [true,"userId field can't be empty!"]
+  },
+  allBids: {
+    type: Array,
+    default: []
+  },
 });
 
 export default mongoose.model("Product", productSchema);
